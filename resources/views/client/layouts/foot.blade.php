8aeacf9f4dcd3560ec00758ab9a423952c63e9ff
Amin Mohamadi <amin.mohamadi13238@gmail.com>
2026-02-17 11:03:54 +0330

    update routes

---
+++ resources/views/client/layouts/foot.blade.php
@@ -0,0 +1,5 @@
+<script src="{{asset('assets/js/dependencies/alpinejs.min.js')}}"></script>
+<script src="{{asset('assets/js/dependencies/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('assets/js/dependencies/plyr.min.js')}}"></script>
+<link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+<script src="{{asset('assets/js/app.js')}}"></script>
---
+++ resources/views/client/layouts/head.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="{{asset('assets/images/favicon.svg')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/dependencies/swiper-bundle.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/dependencies/plyr.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/fonts.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}" />
+{{--    <title>قالب آموزشی نابغه - پروفایل - داشبورد</title>--}}
+</head>
